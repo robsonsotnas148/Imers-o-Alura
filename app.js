0bf84e93341451b19a93f6657c0ecca0949b6060
Robson Santos <80293995+robsonsotnas148@users.noreply.github.com>
2024-09-09 19:18:21 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+    //para adicionar comentarios
+    
+    function pesquisar () {
+        let section = document.getElementById("resultados-pesquisa");
+
+        let campoPesquisa = document.getElementById("campo-pesquisa");
+
+        <div class="item-resultado">
+                <h3><a href="https://pt.wikipedia.org/wiki/Internet_no_Brasil" target="_blank">${dado.Titulo}</a></h3>
+                <p class="descricao-meta">${dado.descricao}.</p>
+                <h3><a href="https://www.minhaconexao.com.br/ranking" target="_blank">Ranking melhores provedores de
+                        Internet</a></h3>
+                <h3><a href="https://agenciadenoticias.ibge.gov.br/agencia-noticias/2012-agencia-de-noticias/noticias/38307-161-6-milhoes-de-pessoas-com-10-anos-ou-mais-de-idade-utilizaram-a-internet-no-pais-em-2022#:~:text=A%20regi%C3%A3o%20com%20maior%20percentual,as%2027%20Unidades%20da%20Federa%C3%A7%C3%A3o."
+                        target="_blank">Regiões com maiores usuarios de redes</a></h3>
+                <p class="descricao-meta">A Internet se popularizou rapidamente, com a expansão das conexões comerciais
+                    e o surgimento de provedores de acesso. Hoje, o Brasil é um dos países com maior número de usuários
+                    de internet no mundo, com a rede desempenhando um papel fundamental na vida social, econômica e
+                    cultural dos brasileiros.</p>
+                <h3 class="item-resultado">3 principais provedoras de internet do Brasil são: </h3>
+                <h3>Vivo</h3>
+                <p class="descricao-meta">A Vivo, uma das maiores empresas de telecomunicações do Brasil, possui uma história rica e marcada por importantes marcos na evolução das telecomunicações no país.
+                    A história da Vivo é uma jornada de crescimento e inovação, marcada pela busca constante por oferecer as melhores soluções em telecomunicações para seus clientes. Desde sua origem, a empresa tem se destacado pela qualidade de seus serviços e pela sua capacidade de se adaptar às mudanças do mercado.
+                </p>
+                <h3>Vtal</h3>
+                <p class="descricao-meta">A V.tal é uma empresa relativamente nova no cenário das telecomunicações brasileiras, mas com uma história e um papel cruciais na transformação digital do país.
+                    A V.tal representa um novo capítulo na história das telecomunicações brasileiras. Com sua vasta rede de fibra ótica e seu foco em infraestrutura, a empresa desempenha um papel fundamental na expansão da conectividade e no desenvolvimento do país. A V.tal é um exemplo de como a infraestrutura de telecomunicações pode ser um catalisador para o crescimento e a inovação.
+                </p>
+                <h3>Claro</h3>
+                <p class="descricao-meta">
+                    A Claro, uma das maiores empresas de telecomunicações do Brasil, possui uma história marcada por fusões, aquisições e inovações que a transformaram em uma das principais operadoras do país.
+                    A história da Claro é marcada pela sua capacidade de se adaptar às mudanças do mercado, pela busca constante por inovação e pela sua dedicação em oferecer serviços de qualidade para seus clientes. A empresa desempenha um papel fundamental na conectividade do Brasil, contribuindo para o desenvolvimento do país.
+                </p>
+            </div>
+    
+}
+
+ 
---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        Titulo : "Vivo",
+        descricao : "A Vivo, uma das maiores empresas de telecomunicações do Brasil, possui uma história rica e marcada por importantes marcos na evolução das telecomunicações no país.A história da Vivo é uma jornada de crescimento e inovação, marcada pela busca constante por oferecer as melhores soluções em telecomunicações para seus clientes. Desde sua origem, a empresa tem se destacado pela qualidade de seus serviços e pela sua capacidade de se adaptar às mudanças do mercado.",
+        link : "https://vivo.com.br/para-voce?gad_source=1&gclid=Cj0KCQjw0Oq2BhCCARIsAA5hubVBn74okjYAb-61I39c_iJ_n0s543iVqASBVa7wwuwXyVE6tZe6Y38aAkpKEALw_wcB&gclsrc=aw.ds"
+    },
+    {
+        Titulo : "Claro",
+        descricao : "A Claro, uma das maiores empresas de telecomunicações do Brasil, possui uma história marcada por fusões, aquisições e inovações que a transformaram em uma das principais operadoras do país.A história da Claro é marcada pela sua capacidade de se adaptar às mudanças do mercado, pela busca constante por inovação e pela sua dedicação em oferecer serviços de qualidade para seus clientes. A empresa desempenha um papel fundamental na conectividade do Brasil, contribuindo para o desenvolvimento do país.",
+        link : "https://www.claro.com.br/"
+    },
+    {
+        Titulo : "Vtal",
+        descricao : "A V.tal é uma empresa relativamente nova no cenário das telecomunicações brasileiras, mas com uma história e um papel cruciais na transformação digital do país.A V.tal representa um novo capítulo na história das telecomunicações brasileiras. Com sua vasta rede de fibra ótica e seu foco em infraestrutura, a empresa desempenha um papel fundamental na expansão da conectividade e no desenvolvimento do país. A V.tal é um exemplo de como a infraestrutura de telecomunicações pode ser um catalisador para o crescimento e a inovação.",
+        link : "https://vtal.com/"
+    }
+];
